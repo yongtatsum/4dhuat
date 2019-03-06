3f535e5f5af5578a3d2861450c29c4641b08ff63
yongtat <yongtat@localhost>
2019-03-06 10:58:45 +0800

    add admob header

--- app/views/layout/bizStore.blade.php
+++ app/views/layout/bizStore.blade.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-3166030118349880",
+          enable_page_level_ads: true
+     });
+</script>
     <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-T8J8N7R');</script>
+    
+    
         <!-- End Google Tag Manager -->
         <meta property='og:type' content="website"/>
     <meta property='og:title' content="4d"/>
