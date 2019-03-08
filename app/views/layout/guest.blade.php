d7384382bd5a191fb75d178a47ab4066e142b24b
yongtat <yongtat@localhost>
2019-03-08 14:35:48 +0800

    add google ads

--- app/views/layout/guest.blade.php
+++ app/views/layout/guest.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-             google_ad_client: "ca-pub-3166030118349880",
-             enable_page_level_ads: true
-        });
-   </script>
+         (adsbygoogle = window.adsbygoogle || []).push({
+              google_ad_client: "ca-pub-3166030118349880",
+              enable_page_level_ads: true
+         });
+    </script>
     <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
